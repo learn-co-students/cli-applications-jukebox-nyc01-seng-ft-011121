08c5f5e2dc647414a0e5ca14100941ef5c331963
Dayne Dewar <daynedewar@yahoo.com>
2021-01-10 02:41:27 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -15,18 +15,32 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
-  if input.is_a? String 
+  if (1..9).to_a.include?(input.to_i)
+    puts "Playing #{songs[input.to_i - 1]}"
+  elsif songs.include?(input)
     puts "Playing #{input}"
-    elsif input.is_a? Integer
-  puts "Invalid input, please try again"
+  else
+    puts "Invalid input, please try again"
   end
 end
 
-def exit
+def exit_jukebox
   puts "Goodbye"
 end
 
-def run
-  puts "PLease enter a command:"
-  input = gets.strip
+def run(songs)
+    puts "Please enter a command:"
+    input = gets.strip
+    if input == 'list'
+      list(songs)
+    elsif input == 'play'
+      list(songs)
+      play(songs)
+    elsif input == 'help'
+      help
+    elsif input == 'exit'
+      exit_jukebox
+    else
+      help
+    end
 end
